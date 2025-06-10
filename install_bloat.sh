948be58eafa50aadb30cc40d4ed58453ca74b5e7
acph <dragopoot@gmail.com>
2025-06-09 18:28:59 -0600

    Adding an script to install a lot of software

---
+++ install_bloat.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pacman -S vi vim neovim strawberry tree toilet tldr stress s-tui ranger pnpm nfs-utils ntfs-3g okular inkscape hdparm feh gimp fish fisher figlet bat base-devel arandr
